d249940dfdcb00aa74cc300e18f7a7fc76eb8cf7
Avery Lychee <averyl@protonmail.com>
2018-05-31 20:58:07 +1000

    Lots of functionality! including integration of Dr Who stuff

--- Main.hs
+++ Main.hs
@@ -8,32 +8,68 @@ import Development.Shake.Util
 indexes :: [FilePath] -> [FilePath]
 indexes = map (\file -> "_site" </> file </> "index.html")
 
+headerGeneric :: FilePath
+headerGeneric = "src/header.html"
+
+
+
 main :: IO ()
 main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
 
-  want (indexes ["", "about", "languages/dapiica"])
+  want (indexes ["", "about", "languages/dapiica", "doctor-who"])
+  want ["_site/style.css"]
+  want ["_site/doctor-who/style.css"]
 
-  "_site/index.html" %> \out -> do
-      let src = "src" </> dropDirectory1 out
-      need [src]
-      cmd_ "cp" [src] [out]
+  phony "clean" $ do
+      putNormal "Cleaning files from _site and _build"
+      removeFilesAfter "_site" ["//*"]
+      removeFilesAfter "_build" ["//*"]
 
   "//*index.html" %> \out -> do
-      let src = "src" </> getMiddle out <.> "md"
-      need [src]
+      case out of
+        "_site/doctor-who/index.html"
+          -> do let db = "src/dw/DrWhoDB"
+                need [db]
+                cmd_ "mkdir -p" (takeDirectory out)
+                command_ [Cwd "src/dw"] "runhaskell" ["DrWho.hs", "output", "../../" ++ out]
+        _
+          -> do let src = case getMiddle out of
+                            "" -> "src/index.md"
+                            name -> "src" </> name <.> "md"
+                let navbar = "_build" </> getMiddle out </> "header.html"
+                let css = generatePrefix (takeDirectory out) ++ "style.css"
+                need [src, navbar]
+                cmd_ "mkdir -p" (takeDirectory out)
+                cmd_ "pandoc --standalone -B" [navbar] "--css" [css] "-o" [out] [src]
+
+  "//*header.html" %> \out -> do
+      let prefix = generatePrefix (takeDirectory out)
       cmd_ "mkdir -p" (takeDirectory out)
-      cmd_ "pandoc --standalone -o" [out] [src]
+      writeFile' out (header prefix)
 
-  -- "_site/*/index.html" %> \out -> do
-  --     let src = "src" </> getMiddle out <.> "md"
-  --     need [src]
-  --     cmd_ "mkdir -p" (takeDirectory out)
-  --     cmd_ "pandoc --standalone -o" out [src]
+  "_site/style.css" %> \out -> do
+      let css = "src/style.css"
+      copyFile' css out
 
-  -- "_site/languages/dapiica/index.html" %> \out -> do
-  --     cmd_ "mkdir -p" (takeDirectory out)
-  --     cmd_ "echo fake" (">" ++ out)
+  "_site/doctor-who/style.css" %> \out -> do
+      let css = "src/dw/style.css"
+      copyFile' css out
 
 
 
+getMiddle :: FilePath -> FilePath
 getMiddle string = dropDirectory1 $ takeDirectory string
+
+countLayers s = length (filter (=='/') s)
+
+generatePrefix s = generatePrefix' (countLayers s)
+  where
+    generatePrefix' 0 = "./"
+    generatePrefix' n = concat $ replicate n "../"
+
+header prefix = "<div id=\"header\">\n"
+                ++ "<div id=navigation>\n"
+                ++ "<a href=\"" ++ prefix ++ "\">Home</a>\n"
+                ++ "<a href=\"" ++ prefix ++ "languages/dapiica\">Dapiica</a>\n"
+                ++ "<a href=\"" ++ prefix ++ "about\">About</a>\n"
+                ++ "</div></div>\n"
---
+++ src/languages/dapiica.md
@@ -0,0 +1,5 @@
+---
+title: Dapiica
+---
+
+wow!
