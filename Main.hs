d4a5228fcb3f1541463a88e96b4ecee750632fb8
Avery Lychee <averyl@protonmail.com>
2018-05-31 18:54:39 +1000

    basic functionality

--- Main.hs
+++ Main.hs
@@ -5,11 +5,35 @@ import Development.Shake.Command
 import Development.Shake.FilePath
 import Development.Shake.Util
 
+indexes :: [FilePath] -> [FilePath]
+indexes = map (\file -> "_site" </> file </> "index.html")
+
 main :: IO ()
 main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
-  want ["_build/index.html"]
 
-  "_build/index.html" %> \out -> do
-      let src = "site" </> dropDirectory1 out
+  want (indexes ["", "about", "languages/dapiica"])
+
+  "_site/index.html" %> \out -> do
+      let src = "src" </> dropDirectory1 out
       need [src]
       cmd_ "cp" [src] [out]
+
+  "//*index.html" %> \out -> do
+      let src = "src" </> getMiddle out <.> "md"
+      need [src]
+      cmd_ "mkdir -p" (takeDirectory out)
+      cmd_ "pandoc --standalone -o" [out] [src]
+
+  -- "_site/*/index.html" %> \out -> do
+  --     let src = "src" </> getMiddle out <.> "md"
+  --     need [src]
+  --     cmd_ "mkdir -p" (takeDirectory out)
+  --     cmd_ "pandoc --standalone -o" out [src]
+
+  -- "_site/languages/dapiica/index.html" %> \out -> do
+  --     cmd_ "mkdir -p" (takeDirectory out)
+  --     cmd_ "echo fake" (">" ++ out)
+
+
+
+getMiddle string = dropDirectory1 $ takeDirectory string
